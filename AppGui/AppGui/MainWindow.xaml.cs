b759eb48d9cd7c728e35c17f2f1207e6111ff787
tiago alexandre melo almeida <tiagomeloalmeida@ua.pt>
2018-03-31 01:06:25 +0100

    First phrase

--- AppGui/AppGui/MainWindow.xaml.cs
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Shapes;
 using System.Xml.Linq;
 using mmisharp;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace AppGui
 {
@@ -16,14 +18,24 @@ namespace AppGui
     public partial class MainWindow : Window
     {
         private MmiCommunication mmiC;
+        private HttpClient ementasClient;
+        private HttpClient sacClient;
+        private HttpClient sasClient;
+    
+        private Tts t;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            t = new Tts();
 
-            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC = new MmiCommunication("localhost", 8000, "User1", "GUI");
             mmiC.Message += MmiC_Message;
             mmiC.Start();
+
+            ementasClient = new HttpClient();
+            ementasClient.BaseAddress = new Uri("http://services.web.ua.pt/sas/ementas");
 
         }
 
@@ -37,7 +49,10 @@ namespace AppGui
             Shape _s = null;
             switch ((string)json.recognized[0].ToString())
             {
-                case "SQUARE": _s = rectangle;
+                case "CANTEENS":
+                    string queryParams = "?format=json";
+                    Console.WriteLine("CANTINASSSASAS");
+                    ementasClient.GetStringAsync(queryParams).ContinueWith((response)=>handleCanteenResponse(response.Result, (string)json.recognized[2].ToString()));
                     break;
                 case "CIRCLE": _s = circle;
                     break;
@@ -60,9 +75,23 @@ namespace AppGui
                         break;
                 }
             });
-            
 
 
+
+        }
+
+        private void handleCanteenResponse(string response, string canteen) {
+
+            dynamic json = JsonConvert.DeserializeObject(response);
+
+            foreach (dynamic canteenResponse in json.menus.menu) {
+                //just testing
+                if ((canteenResponse["@attributes"].canteen.ToString().ToUpper().Contains(canteen) == true)) { 
+                    Console.WriteLine("PARSE!!");
+                    t.Speak(canteenResponse["@attributes"].disabled.ToString());
+                    break;
+                }
+            }
         }
     }
 }
