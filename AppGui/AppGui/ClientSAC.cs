bab317125842da7479baa6c1f12f6f765d2646f7
Unknown <anafilipatavares@ua.pt>
2018-04-02 16:32:59 +0100

    Skeleton to ua attendance tickets -- missing api call

---
+++ AppGui/AppGui/ClientSAC.cs
@@ -0,0 +1,72 @@
+﻿﻿using AppGui.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGui
+{
+    class ClientSAC
+    {
+        private HttpClient client;
+        private DialogueManager dManager;
+
+
+        public ClientSAC(DialogueManager dManager)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://services.web.ua.pt/sac/senhas");
+            this.dManager = dManager;
+        }
+
+        public void request(string[] args)
+        {
+            string queryParams = "?format=json";
+
+            if (args.Length == 1 && args[0].ToString().Equals("TYPE1"))
+                client.GetStringAsync(queryParams).ContinueWith((response) => handleResponse(response.Result, args));
+
+            else
+            {
+                queryParams += "&letter=" + args[1];
+                client.GetStringAsync(queryParams).ContinueWith((response) => handleSingleResponse(response.Result, args));
+
+            }
+        }
+
+        private void handleSingleResponse(string response, string[] args)
+        {
+            dynamic json = JsonConvert.DeserializeObject(response);
+
+            Console.WriteLine(json.ToString());
+
+            dManager.manageDialogueSAC(getTicket(json, args[1]), args);
+        }
+
+        private void handleResponse(string response, string[] args)
+        {
+
+            dynamic json = JsonConvert.DeserializeObject(response);
+
+            Console.WriteLine(json.ToString());
+
+            // TODO 
+            dManager.manageDialogueSAC(getAllTicketsInfo(json), args);
+
+        }
+
+        private TicketData getTicket(dynamic json, string letter)
+        {
+            return new TicketData(letter, false);
+        }
+
+        private List<TicketData> getAllTicketsInfo(dynamic json)
+        {
+            List<TicketData> list = new List<TicketData>();
+            return list;
+        }
+    }
+}
---
+++ AppGui/AppGui/Data/TicketData.cs
@@ -0,0 +1,34 @@
+﻿namespace AppGui.Data
+{
+    public class TicketData
+    {
+
+        private string letter;
+        private string description;
+        private int latest;
+        private int averageAtendingTime; // ver se minutos ou segundos  
+        private int averageWaitingTime;
+        private int clientsWaiting;
+        private bool found;
+
+        public TicketData(string letter, bool found)
+        {
+            this.letter = letter;
+            this.found = found;
+        }
+
+        public string Letter { get => letter; set => letter = value; }
+
+        public string Description { get => description; set => description = value; }
+
+        public int Latest { get => latest; set => latest = value; }
+
+        public int AverageAtendingTime { get => averageAtendingTime; set => averageAtendingTime = value; }
+
+        public int AverageWaitingTime { get => averageWaitingTime; set => averageWaitingTime = value; }
+
+        public int ClientsWaiting { get => clientsWaiting; set => clientsWaiting = value; }
+
+        public bool Found { get => found; set => found = value; }
+    }
+}
