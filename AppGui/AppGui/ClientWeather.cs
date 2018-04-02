241726d3d16c84962033f1b28d8324747b0af1c1
Unknown <anafilipatavares@ua.pt>
2018-04-03 00:16:11 +0100

    Start weather

---
+++ AppGui/AppGui/ClientWeather.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGui
+{
+    class ClientWeather
+    {
+        private HttpClient client;
+        private DialogueManager dManager;
+
+
+        public ClientWeather(DialogueManager dManager)
+        {
+            client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/forecast/daily?appid=bd5e378503939ddaee76f12ad7a97608&id=2742611&units=metric&lang=pt");
+            this.dManager = dManager;
+        }
+
+        public void request(string[] args)
+        {
+            client.GetStringAsync("").ContinueWith((response) => handleResponse(response.Result, args));
+        }
+
+        private void handleResponse(string result, object args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppGui/AppGui/Data/weatherData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGui.Data
+{
+    class WeatherData
+    {
+
+        private int minTemp;
+        private int maxTemp;
+        private int windSpeed;
+        private int humidity;
+        private string description;
+
+        private string dayDescription;   // hoje, amanhã, na segunda, dia 23
+        private DateTime date;
+
+        public int MinTemp { get => minTemp; set => minTemp = value; }
+        public int MaxTemp { get => maxTemp; set => maxTemp = value; }
+        public int WindSpeed { get => windSpeed; set => windSpeed = value; }
+        public int Humidity { get => humidity; set => humidity = value; }
+        public string Description { get => description; set => description = value; }
+        public string DayDescription { get => dayDescription; set => dayDescription = value; }
+        public DateTime Date { get => date; set => date = value; }
+    }
+}
