e50497db8082131074b074e3b8c121223cb109c1
Tiago Almeida <tiagoalmeida.6.8.1996@gmail.com>
2018-04-14 18:49:02 +0100

    news and parks with handler for connection errors

--- AppGui/AppGui/ClientNews.cs
+++ AppGui/AppGui/ClientNews.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,23 +20,55 @@ namespace AppGui
         {
             client = new HttpClient();
             client.BaseAddress = new Uri("http://services.sapo.pt/UA/Online/contents_xml");
+            client.Timeout = TimeSpan.FromSeconds(6);
+
             this.dManager = dManager;
         }
 
         public void request(string[] args) {
-            string queryParams = "?jsonText=true";
 
-            //antes tinha cache das noticas mas n garantia que estavam sempre updated
-            client.GetByteArrayAsync(queryParams).ContinueWith((response) => handle(Encoding.UTF8.GetString(response.Result), args));
-            
-            
-            //client.GetStringAsync(queryParams).ContinueWith((response) => handle(response.Result));
+            getResponse(args);
         }
 
-        private void handle(string response, string[] args)
+        async void getResponse(string[] args)
+        {
+            try
+            {
+                Task<byte[]> getResponseTask = client.GetByteArrayAsync("?jsonText=true");
+
+                anotherTask(getResponseTask);
+
+                byte[] response = await getResponseTask;
+                handleResponse(Encoding.UTF8.GetString(response), args);
+
+            }
+            catch (HttpRequestException e)
+            {
+                if (e.InnerException is WebException)
+                {
+                    dManager.manageDialogueWeatherConnectionErrors("web exception", "das senhas académicas");
+                }
+
+            }
+            catch (TaskCanceledException e)
+            {
+                dManager.manageDialogueWeatherConnectionErrors("timeout", "das senhas académicas");
+            }
+        }
+
+        private async void anotherTask(Task<byte[]> getResponseTask)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            if (!getResponseTask.IsCompleted)
+            {
+                dManager.manageDialogueWeatherConnectionErrors("warning timeout", "das senhas académicas");
+            }
+        }
+
+        private void handleResponse(string response, string[] args)
         {
             Console.WriteLine("Handle");
-
+            Console.WriteLine(response);
             dynamic json = JsonConvert.DeserializeObject(response);
 
             List<NewsData> newsList = new List<NewsData>();
